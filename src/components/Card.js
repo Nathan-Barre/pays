48c062aad54eb605ca5386ffa6095d63c275c0be
Nathan <nathan.barre@epitech.eu>
2022-04-23 17:46:20 +0200

    init

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({country}) => {
+    return(
+        <li className="card">
+            <img src={country.flags.svg} alt={"Drapeau " + country.translations.fra.common}/>
+            <div className="infos">
+                <h2>{country.translations.fra.common}</h2>
+                <h2>{country.capital}</h2>
+                <h2>Pop: {country.population.toLocaleString()}</h2>
+            </div>
+        </li>
+    )
+}
+
+export default Card;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation />
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum 1</p>
+            <br/>
+            <p>Lorem ipsum 2</p>
+        </div>
+    )
+}
+
+export default About;
